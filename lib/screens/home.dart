0a9b9b78f221d0b7455b316a3f4748e44c54b4cb
Chanin Nusang <6110110589@psu.ac.th>
2021-08-13 16:13:18 +0700

    hive generate adapter

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:last_time_mobile_midterm/models/lasttime.dart';
 import 'package:last_time_mobile_midterm/screens/history.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -9,6 +11,23 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var box = Hive.box('lasttime');
+  List<LastTime> lastTime = [];
+  List<String> category = {}
+  @override
+  void initState() {
+    // pullLastTime();
+    super.initState();
+  }
+
+  // void pullLastTime() async {
+  //   try {
+  //     lastTime = await box.get('lasttime');
+  //   } catch (e) {
+
+  //   }
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,14 +42,49 @@ class _HomeState extends State<Home> {
           actions: [
             IconButton(
               icon: Icon(Icons.history),
-              onPressed: () {},
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => History()),
+                );
+              },
             )
           ],
         ),
         body: Container(
-          child: Center(
-            child: Text('Home'),
+          child: Column(
+            children: [
+              Row(
+                children: [],
+              ),
+              ListView.builder(
+                  physics: BouncingScrollPhysics(),
+                  itemCount: box.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    LastTime _lastTime = box.get(index + 1);
+                    return Wrap(
+                      children: [
+                        LasttimeTile(lasttime: _lastTime),
+                        if (index < box.length - 1)
+                          Padding(
+                            padding: const EdgeInsets.only(left: 16, right: 16),
+                            child: Divider(),
+                          )
+                      ],
+                    );
+                  })
+            ],
           ),
         ));
+  }
+}
+
+class LasttimeTile extends StatelessWidget {
+  LasttimeTile({@required this.lasttime});
+  final lasttime;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
